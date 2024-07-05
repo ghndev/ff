f001bf9a26f343faf53d0e6325e27007c233649f
Kwak Hoon <77324409+hooncdn@users.noreply.github.com>
2024-07-05 12:21:11 +0900

    feat: Add Watchlist and User navigation links
    - Implement Watchlist link with Bookmark icon
    - Add User link with custom SVG user icon
    - Style links with custom gray color and flex layout
    - Ensure consistent spacing and alignment of icons and text

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import MaxWidthWrapper from './max-width-wrapper'
-import { Search } from 'lucide-react'
 import SearchBar from './search-bar'
+import { Bookmark, CircleUser } from 'lucide-react'
 
 export default function Navbar() {
   return (
@@ -15,25 +15,44 @@ export default function Navbar() {
           <div className="flex items-center space-x-7 text-custom-gray">
             <Link href="/movies">Movies</Link>
             <Link href="/tv">TV Shows</Link>
-            <Link href="/movies">Watch</Link>
+            <Link href="/trailers">Trailers</Link>
           </div>
 
           <SearchBar />
-          {/* <div className="relative">
-            <select className="appearance-none bg-[#242424] text-gray-300 py-2 pl-3 pr-8 rounded-l-md border-r border-gray-700 focus:outline-none">
-              <option>All</option>
-              <option>Movies</option>
-              <option>TV Shows</option>
-            </select>
-            <input
-              type="text"
-              placeholder="Search FlickFocus"
-              className="w-full bg-[#242424] text-white py-2 px-4 focus:outline-none"
-            />
-            <button className="absolute right-0 top-0 h-full px-4 bg-[#242424] rounded-r-md">
-              <Search className="text-gray-400" />
-            </button>
-          </div> */}
+
+          <Link href="watchlist" className="text-custom-gray flex items-center">
+            <Bookmark className="h-6 w-6 fill-custom-gray mr-1" /> Watchlist
+          </Link>
+
+          <Link href="sign-in" className="text-custom-gray flex items-center">
+            <svg
+              width="24"
+              height="25"
+              viewBox="0 0 24 25"
+              fill="none"
+              className="mr-1"
+              xmlns="http://www.w3.org/2000/svg">
+              <g clip-path="url(#clip0_2042_2085)">
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M12 23.5C18.0751 23.5 23 18.5751 23 12.5C23 6.42487 18.0751 1.5 12 1.5C5.92487 1.5 1 6.42487 1 12.5C1 18.5751 5.92487 23.5 12 23.5ZM14.5629 9.07143C14.5629 10.4943 13.4229 11.6429 12 11.6429C10.5771 11.6429 9.42857 10.4943 9.42857 9.07143C9.42857 7.64857 10.5771 6.5 12 6.5C13.4229 6.5 14.5629 7.64857 14.5629 9.07143ZM6 16.3571C6 14.36 10.0029 13.3571 12 13.3571C13.9971 13.3571 18 14.36 18 16.3571V17.6429C18 18.1143 17.6143 18.5 17.1429 18.5H6.85714C6.38571 18.5 6 18.1143 6 17.6429V16.3571Z"
+                  fill="#C3C3C3"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0_2042_2085">
+                  <rect
+                    width="24"
+                    height="24"
+                    fill="white"
+                    transform="translate(0 0.5)"
+                  />
+                </clipPath>
+              </defs>
+            </svg>
+            User
+          </Link>
         </div>
       </MaxWidthWrapper>
     </div>
